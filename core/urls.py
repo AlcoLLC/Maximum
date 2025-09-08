19bd4ab323f642a50453bc4bc12329ee4200198b
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-08 14:35:30 +0400

    news

--- core/urls.py
+++ core/urls.py
@@ -61,10 +61,8 @@ urlpatterns += [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
 handler404 = 'home.views.handler404'
 handler403 = 'home.views.handler403'
