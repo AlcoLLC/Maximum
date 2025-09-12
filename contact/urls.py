d08bf92f8a3e7e9326718fc95fe6a8d517fae636
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-12 11:29:22 +0400

    contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import contact_view, contact_step_two_view, validate_email_ajax
+from .views import contact_info_view, contact_step_two_view, validate_email_ajax
 
 app_name = "contact"
 
 urlpatterns = [
-    path("contact/", contact_view, name="contact"),
+    path("contact/", contact_info_view, name="contact"),
     path('contact-step-2/', contact_step_two_view, name='contact_step_two'),
     path('validate-email/', validate_email_ajax, name='validate_email'),
 ]
