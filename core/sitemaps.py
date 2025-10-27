34786badb8e419bee93ad5725a8bed0a2d75d5c7
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-10-27 10:10:58 +0400

    sitemap

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -1,30 +1,75 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from django.utils import timezone
+from product.models import Product
+from news.models import News
 
+class HomeSitemap(Sitemap):
+    changefreq = 'daily' 
+    priority = 1.0
 
-class StaticViewSitemap(Sitemap):
-    priority = 0.8
-    changefreq = 'monthly'
-    
     def items(self):
-        return [
-            'home:home',
-            'about:about',
-            'product:products',
-            'services:services', 
-            'contact:contact',
-            'partnership:partnership',
-            'news:news',
-            'faq:faq'
-        ]
-    
+        return ['home:home']
+
     def location(self, item):
         return reverse(item)
 
 
+class ProductSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        return Product.objects.all()
+
+    def location(self, obj):
+        return reverse('product:product_detail', kwargs={'slug': obj.slug})
+
+
+class NewsSitemap(Sitemap):
+    changefreq = 'daily' 
+    priority = 0.8
+
+    def items(self):
+        return News.objects.filter(is_active=True)
+
+    def location(self, obj):
+        return reverse('news:news_detail', kwargs={'slug': obj.slug})
+
+class StaticViewSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.7
+
+    def items(self):
+        return [
+            'about:about',
+            'brands:brands',
+            'contact:contact',
+            'faq:faq',
+            'news:news', 
+            'partnership:partnership',
+            'product:products',
+            'services:services',
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class ContactFormSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.4
+    
+    def items(self):
+        return ['contact:contact_step_two']
+        
+    def location(self, item):
+        return reverse(item)
 
 
 sitemaps = {
-    'static': StaticViewSitemap,
+    'home': HomeSitemap,
+    'static-pages': StaticViewSitemap,
+    'products': ProductSitemap,
+    'news': NewsSitemap,
+    'contact-form': ContactFormSitemap,
 }
